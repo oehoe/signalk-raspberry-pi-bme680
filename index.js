9b9691f6eb8a76dad5f361c1dae4880dc4a8c8e1
romton843 <53725060+romton843@users.noreply.github.com>
2021-01-13 23:47:17 -0500

    Update index.js

--- index.js
+++ index.js
@@ -19,12 +19,49 @@ module.exports = function (app) {
       rate: {
         title: "Sample Rate (in seconds)",
         type: 'number',
-        default: 60
+        description: 'You may consider a longer time but be aware that the burn in periode for the air quality requires at least 60 readings.',
+        default: 5
       },
-      path: {
+      burn_in_time: {
+        title: 'Burn in time (in seconds)',
+        description: 'Defines the time the sensor will heat up until using the gas data for air quality index',
+        type: 'number',
+        default: 500
+      },
+      hum_baseline: {
+        title: 'Humidity baseline (in %)',
+        description: '40% is considered to be an optimal indoor humidity',
+        type: 'number',
+        default: 40
+      },
+      hum_weighting: {
+        title: 'Humidity weighting (in %)',
+        description: 'This Sets the balance between humidity and gas reading in the calculation of air_quality_index. Default: 25:75, humidity:gas',
+        type: 'number',
+        default: 25
+      },
+      path1: {
         type: 'string',
-        title: 'SignalK Path',
-        description: 'This is used to build the path in Signal K. It will be appended to \'environment\'',
+        title: 'SignalK Path for temperature',
+        description: 'This is used to build the path in Signal K for the temperature. It will be appended to \'environment\'',
+        default: 'inside'
+      },
+      path2: {
+        type: 'string',
+        title: 'SignalK Path for humidity',
+        description: 'This is used to build the path in Signal K for the humidity. It will be appended to \'environment\'',
+        default: 'inside'
+      },
+      path3: {
+        type: 'string',
+        title: 'SignalK Path for pressure',
+        description: 'This is used to build the path in Signal K for the pressure. It will be appended to \'environment\'',
+        default: 'inside'
+      },
+      path4: {
+        type: 'string',
+        title: 'SignalK Path for gas and air quality',
+        description: 'This is used to build the path in Signal K for the gas and air quality. It will be appended to \'environment\'',
         default: 'inside'
       },
       i2c_bus: {
@@ -42,7 +79,7 @@ module.exports = function (app) {
 
   plugin.start = function (options) {
     
-    function createDeltaMessage (temperature, humidity, pressure) {
+    function createDeltaMessage (temperature, humidity, pressure, gas_resistance, air_quality_index) {
       return {
         'context': 'vessels.' + app.selfId,
         'updates': [
@@ -61,6 +98,12 @@ module.exports = function (app) {
               }, {
                 'path': 'environment.' + options.path + '.pressure',
                 'value': pressure
+              }, {
+                'path': 'environment.' + options.path4 + '.gas_resistance',
+                'value': gas_resistance
+              }, {
+                'path': 'environment.' + options.path4 + '.air_quality_index',
+                'value': air_quality_index
               }
             ]
           }
@@ -71,6 +114,9 @@ module.exports = function (app) {
     const bme680 = new Bme680(options.i2c_bus || 1, Number(options.i2c_address || '0x77'));
     //const bme680 = new Bme680(1, 0x77);
 
+    var gas_baseline = 0;
+    var burn_in_data = [];
+    
     // Read BME680 sensor data
     function readSensorData() {
 
@@ -82,15 +128,48 @@ module.exports = function (app) {
               temperature = sensorData.data.temperature + 273.15;
               pressure = sensorData.data.pressure * 100;
               humidity = sensorData.data.humidity;
+              gas_resistance = sensorData.data.gas_resistance;
+              hum_offset = humidity - options.hum_baseline;
+	      
+              // calculate hum_score as the distance from the hum_baseline
+              if (hum_offset > 0) {
+                  hum_score = (100 - options.hum_baseline - hum_offset) / (100 - options.hum_baseline) * (options.hum_weighting);
+              } else {
+                  hum_score = (options.hum_baseline + hum_offset) / options.hum_baseline * (options.hum_weighting);
+              }
+              
+              // calculate gas baseline     
+              if ((burn_in_data.length < (options.burn_in_time / options.rate)) || (burn_in_data.length < 60)) {
+                  burn_in_data.push(gas_resistance);
+                  gas_offset = 0;
+              } else if (gas_baseline == 0) {
+                  gas_baseline = burn_in_data.slice(-50).reduce((a, b) => a + b) / burn_in_data.slice(-50).length;
+                  gas_offset = gas_baseline - gas_resistance;
+              }
 
+              // calculate gas_score as the distance from the gas_baseline
+              if (gas_baseline > 0) {
+                  gas_offset = gas_baseline - gas_resistance;
+              }
+              if (gas_offset > 0) {
+                  gas_score = (gas_resistance / gas_baseline) * (100 - options.hum_weighting);
+              } else {
+                gas_score = 100 - options.hum_weighting;
+              }
+
+              // calculate air_quality_score
+              air_quality_score = hum_score + gas_score;
+              // convert to Air Quality Index
+              air_quality_index = Math.round(500 - (5 * air_quality_score));
+              
               // create message
-              var delta = createDeltaMessage(temperature, humidity, pressure);
+              var delta = createDeltaMessage(temperature, humidity, pressure, gas_resistance, air_quality_index);
 
               // send data
               app.handleMessage(plugin.id, delta);
 
             } catch(err) {
-              var delta = createDeltaMessage(null, null, null);
+              var delta = createDeltaMessage(null, null, null, null, null);
               app.handleMessage(plugin.id, delta);
             }
 
@@ -98,7 +177,7 @@ module.exports = function (app) {
       })
       .catch((err) => {
         app.debug('Unable to initialize BME680');
-        var delta = createDeltaMessage(null, null, null);
+        var delta = createDeltaMessage(null, null, null, null, null);
         app.handleMessage(plugin.id, delta);
       });
     }
